37ab2fe945f674b1a8a48f040f49d3d4e98814f1
Robert Rea <robert.rea@firehost.com>
2017-11-07 08:30:47 -0600

    t

---
+++ Interview/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Interview.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        // Return in JSON Format the Person objects with an object under them that contains their pet object.
+        // Create Unit Tests
+
+        [HttpGet]
+        public string Get()
+        {
+            var personsRaw = System.IO.File.ReadAllText(@"C:\Projects\Interview\Interview\data\person.txt");
+            var petsRaw = System.IO.File.ReadAllText(@"C:\Projects\Interview\Interview\data\pet.txt");
+
+            return personsRaw;
+        }
+        
+        // Create a Search endpoint that takes in a part of a persons name and returns a person object their pet objects.
+    }
+}
